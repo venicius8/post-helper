026bd8fbac21d00c4a49cd0a98cb7d2775a11710
Venícius <miguelsilva8v@gmail.com>
2025-08-14 14:47:15 -0300

    feat: add emojis to tasks

--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ const headerOptions = document.getElementById("headerOptions");
 const headerInputEl = document.getElementById("headerInput");
 
 const createTask = document.getElementById("createTask");
+let taskList;
 
 // Create task button
 
@@ -23,7 +24,16 @@ createTask.addEventListener("click", () => {
   newTaskTime.min = "0.5";
   newTaskTime.placeholder = "Duração (em horas)";
 
-  document.getElementById("tasks").append(newTaskName, newTaskTime);
+  const newTaskEmoji = document.createElement("input");
+  newTaskEmoji.className = "taskEmoji";
+  newTaskEmoji.maxLength = "2";
+  newTaskEmoji.placeholder = "Escolha um emoji";
+  newTaskEmoji.onfocus = "emoji()";
+  newTaskEmoji.onblur = "emoji()";
+
+  document
+    .getElementById("tasks")
+    .append(newTaskEmoji, newTaskName, newTaskTime);
 });
 
 // Converting days
@@ -38,15 +48,16 @@ currentDayDisplay.innerText = `Dia ${currentDay} de 100 #100DaysOfCode`;
 document.getElementById("editor").addEventListener("input", () => {
   const tasksNameEl = document.querySelectorAll(".taskName");
   const tasksTimeEl = document.querySelectorAll(".taskTime");
+  const tasksEmojiEl = document.querySelectorAll(".taskEmoji");
 
   let headerInput = headerInputEl.value;
   if (headerOptions.value === "phrase") {
     headerInput = `- "${headerInput}"`;
   }
 
-  let taskList = "";
+  taskList = "";
   for (let i = 0; i < tasksNameEl.length; i++) {
-    taskList += `${tasksTimeEl[i].value}h - ${tasksNameEl[i].value}\n\n`;
+    taskList += `${tasksEmojiEl[i].value} ${tasksTimeEl[i].value}h - ${tasksNameEl[i].value}\n\n`;
   }
 
   preview.innerText = `Dia ${currentDay} de 100 #100DaysOfCode
@@ -56,3 +67,15 @@ document.getElementById("editor").addEventListener("input", () => {
     ${taskList}
   `;
 });
+
+document
+  .querySelector("emoji-picker")
+  .addEventListener("emoji-click", (event) => {
+    taskList += event.detail.unicode;
+  });
+
+function emoji() {
+  setTimeout(() => {
+    document.querySelector("emoji-picker").classList.toggle("emojis-active");
+  }, 100);
+}
