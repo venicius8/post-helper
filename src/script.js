5d82eb6e5cbdb347aa252e6954816cd613032c1d
Venícius <miguelsilva8v@gmail.com>
2025-08-14 09:47:17 -0300

    fix: fix createTask bug

--- src/script.js
+++ src/script.js
@@ -1,33 +1,44 @@
+// Imports
+
 const editor = document.getElementById("editor");
 const preview = document.getElementById("preview");
+
 const headerOptions = document.getElementById("headerOptions");
 const headerInputEl = document.getElementById("headerInput");
-const tasksNameEl = document.querySelectorAll(".taskName");
-const tasksTimeEl = document.querySelectorAll(".taskTime");
+
 const createTask = document.getElementById("createTask");
 
 let taskNameList = "";
 
+// Create task button
+
 createTask.addEventListener("click", () => {
   const newTaskName = document.createElement("input");
-  newTaskName.class = "taskName";
+  newTaskName.className = "taskName";
   newTaskName.placeholder = "Qual foi a tarefa";
 
   const newTaskTime = document.createElement("input");
-  newTaskName.class = "taskTime";
-  newTaskName.type = "number";
-  newTaskName.step = "0.5";
-  newTaskName.minlength = "0.5";
-  newTaskName.placeholder = "Duração (em horas)";
+  newTaskTime.className = "taskTime";
+  newTaskTime.type = "number";
+  newTaskTime.step = "0.5";
+  newTaskTime.min = "0.5";
+  newTaskTime.placeholder = "Duração (em horas)";
 
-  document.getElementById("tasks").append(newTaskTime, newTaskName);
+  document.getElementById("tasks").append(newTaskName, newTaskTime);
 });
+
+// Converting days
 
 const dayOne = new Date("2025-08-04");
 const today = new Date();
 const currentDay = Math.floor((today - dayOne) / 1000 / 60 / 60 / 24);
 
+// Listen to editor input
+
 document.getElementById("editor").addEventListener("input", () => {
+  const tasksNameEl = document.querySelectorAll(".taskName");
+  const tasksTimeEl = document.querySelectorAll(".taskTime");
+
   let headerInput = headerInputEl.value;
   if (headerOptions.value === "phrase") {
     headerInput = `- "${headerInput}"`;
@@ -35,7 +46,7 @@ document.getElementById("editor").addEventListener("input", () => {
 
   taskNameList = "";
   for (const taskName of tasksNameEl) {
-    taskNameList += taskName.value + "\n";
+    taskNameList += taskName.value + "\n\n";
   }
 
   preview.innerText = `Dia ${currentDay} de 100 #100DaysOfCode
